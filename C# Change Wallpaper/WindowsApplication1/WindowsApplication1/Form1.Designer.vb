623b3901cb6a03e7802a8a865c45e09ff701a856
Matias Gabriel Paz <Mati.G.Paz@gmail.com>
2019-08-15 12:38:42 -0300

    .
    .

---
+++ C# Change Wallpaper/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxLocation = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnApply = New System.Windows.Forms.Button()
+        Me.btnbrowse = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'TextBoxLocation
+        '
+        Me.TextBoxLocation.Location = New System.Drawing.Point(12, 12)
+        Me.TextBoxLocation.Name = "TextBoxLocation"
+        Me.TextBoxLocation.Size = New System.Drawing.Size(231, 20)
+        Me.TextBoxLocation.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 38)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(231, 155)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = false
+        '
+        'btnApply
+        '
+        Me.btnApply.Location = New System.Drawing.Point(260, 55)
+        Me.btnApply.Name = "btnApply"
+        Me.btnApply.Size = New System.Drawing.Size(75, 23)
+        Me.btnApply.TabIndex = 2
+        Me.btnApply.Text = "Apply"
+        Me.btnApply.UseVisualStyleBackColor = True
+        '
+        'btnbrowse
+        '
+        Me.btnbrowse.Location = New System.Drawing.Point(260, 10)
+        Me.btnbrowse.Name = "btnbrowse"
+        Me.btnbrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnbrowse.TabIndex = 3
+        Me.btnbrowse.Text = "Browse"
+        Me.btnbrowse.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 210)
+        Me.Controls.Add(Me.btnbrowse)
+        Me.Controls.Add(Me.btnApply)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBoxLocation)
+        Me.Name = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents TextBoxLocation As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents btnApply As System.Windows.Forms.Button
+    Friend WithEvents btnbrowse As System.Windows.Forms.Button
+
+End Class
---
+++ C# Change Wallpaper/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+
+    Private Declare Function SystemParametersInfo Lib "user32" Alias "SystemParametersInfoA" (ByVal uAction As Integer, ByVal uParam As Integer, ByVal lpvParam As String, ByVal fuWinIni As Integer) As Integer
+    'Declare two constant
+    Private Const SETDESKWALLPAPER = 20
+    Private Const UPDATEINIFILE = &H1
+
+
+
+    Private Sub btnbrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnbrowse.Click
+        Dim dlg As New OpenFileDialog
+        dlg.Title = "Choose Desktop background image"
+        dlg.Filter = ""
+        If dlg.ShowDialog = Windows.Forms.DialogResult.OK Then
+            TextBoxLocation.Text = dlg.FileName
+            PictureBox1.BackgroundImage = Image.FromFile(dlg.FileName)
+            PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
+        End If
+
+    End Sub
+
+
+
+    Private Sub btnApply_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnApply.Click
+        SystemParametersInfo(SETDESKWALLPAPER, 0, TextBoxLocation.Text, UPDATEINIFILE)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
